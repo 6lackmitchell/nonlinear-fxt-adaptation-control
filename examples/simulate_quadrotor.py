f412b8e307030236b909459aab9ea63a600a9aae
Mitchell Black <6lack.mitchell@gmail.com>
2022-10-25 14:52:53 -0400

    Refactors example files

--- examples/simulate_quadrotor.py
+++ examples/simulate_quadrotor.py
@@ -1,10 +1,10 @@
-from simulate import simulate
+from src.core.simulate import simulate
 
 vehicle = "quadrotor"
 level = "dynamic_6dof"
 situation = "wind_field"
 
 end_time = 40.0
-timestep = 0.001
+timestep = 0.01
 
 success = simulate(end_time, timestep, vehicle, level, situation)
